0ef19893cbea056aa16956ce5f71ff8e5c3199f6
Tami-Ann Grant <61tnargimat@gmail.com>
2025-02-24 17:12:30 -0500

    update

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
@@ -18,17 +19,21 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      },
+  useQuery({
+    queryKey: ["games", gameQuery],
+    queryFn: () => apiClient,
+  });
+useData<Game>(
+  "/games",
+  {
+    params: {
+      genres: gameQuery.genre?.id,
+      platforms: gameQuery.platform?.id,
+      ordering: gameQuery.sortOrder,
+      search: gameQuery.searchText,
     },
-    [gameQuery]
-  );
+  },
+  [gameQuery]
+);
 
 export default useGames;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 export interface Genre {
   id: number;
@@ -6,6 +9,14 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null })
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 * mins * secs * millisecs 24hrs data will be fresh for 24hrs no request will be made to the backend to fetch the genres
+    // So we don't have to go to the backend and show the user a spinner
+    initialData: { count: genres.length, results: genres }, // this is to ensure type matches FetchResponse
+  });
 
 export default useGenres;
